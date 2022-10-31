3afa09bf53d24be0d1594c7fc78f0edd85d19d07
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-10-31 14:55:58 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,19 +14,19 @@ function renderLicenseLink(license) {
     return `* [license](#license)`
   }
   return ""
- }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
 
-    if (license !== "none") {
-      return `## License
-      this project is under license ${license}` 
-    }
-    return ""
+  if (license !== "none") {
+    return `## License
+      this project is under license ${license}`
+  }
+  return ""
 
- }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
